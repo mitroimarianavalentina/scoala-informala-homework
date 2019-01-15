9a568901cef4c6f3795adbda44147f457fad73de
Mary <mitroimarianavalentina@gmail.com>
2019-01-15 20:41:13 +0200

    homework Functions - 09.01.2019

---
+++ Functions09January/src/Exercise1/Max.java
@@ -0,0 +1,53 @@
+
+/**
+ * 1. Calculate Max
+ * - Create a method getMax() with two integer (int) parameters, that returns maximal of the two numbers.
+ * - Write a program that reads three numbers from the console and prints the biggest of them. Use the getMax()
+ *   method you just created.
+ *
+ * @author Maariana Valentina Mitroi
+ */
+
+
+package Exercise1;
+
+import java.util.Scanner;
+
+public class Max {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        exerciseOne();
+    }
+
+    /**
+     * method to get the max number out of two
+     *
+     * @param a first number to compare
+     * @param b second number to compare
+     * @return the biggest number
+     */
+    private static int getMax(int a, int b) {
+        if (a >= b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    /**
+     * method  to find the max from three given numbers, using getMax() method
+     */
+    private static void exerciseOne() {
+        System.out.print("Enter the first number: ");
+        int first = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int second = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int third = scanner.nextInt();
+
+        System.out.print("the biggest from " + first + ", " + second + ", " + third +
+                " is: " + getMax(getMax(getMax(first, second), getMax(first, third)), getMax(second, third)));
+    }
+}
---
+++ Functions09January/src/FahrenheitToCelsius/FahrenheitToCelsius.java
@@ -0,0 +1,52 @@
+
+/**
+ * 2. Fahrenheit to Celsius Conversion
+ * - create a method that converts temperature from Fahrenheit to Celsius.
+ * - write a program that for a given (by the user) body temperature, measured in Fahrenheit degrees,
+ *    outputs it in Celsius degrees, with the following message: "Your body temperature in Celsius degrees is X",
+ *    where X is respectively the Celsius degrees. In addition if the measured temperature in Celsius is higher
+ *    than 37 degrees, the program should warn the user that they are ill, with the following message "You are ill!".
+ *    (use the method created before)
+ *
+ * @author Maariana Valentina Mitroi
+ */
+
+package FahrenheitToCelsius;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static final float NORMAL_BODY_TEMPERATURE = 37.f;
+
+    public static void main(String[] args) {
+
+        exerciseTwo();
+    }
+
+    /**
+     * method to calculate from fahrenheit to celsius
+     *
+     * @param fahrenheit - the fahrenheit value given by the user
+     * @return the value resulted after transforming the fahrenheit to celsius
+     */
+    private static float fahrenheitToCelsius(int fahrenheit) {
+        return (float) (fahrenheit - 32) * 5 / 9;
+    }
+
+    /**
+     * method to display the Celsius temperature using the fahrenheitToCelsius() method
+     */
+    private static void exerciseTwo() {
+        System.out.print("Enter the body temperature (in Fahrenheit): ");
+        int fahrenheit = scanner.nextInt();
+        if (fahrenheitToCelsius(fahrenheit) > NORMAL_BODY_TEMPERATURE) {
+            System.out.println("You are ill!");
+            System.out.print("Your body temperature is: " + fahrenheitToCelsius(fahrenheit) + " Celsius.");
+        } else {
+            System.out.print("Your body temperature is: " + fahrenheitToCelsius(fahrenheit) + " Celsius.");
+        }
+    }
+
+}
